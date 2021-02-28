e38a9a8df03e6e73645e45b96a3bd07c78867191
aaron.leon21 <aaron.leon21@gmail.com>
2021-02-28 04:03:11 -0500

    Small changes to pet details layout and form

--- src/pages/PetDetails.js
+++ src/pages/PetDetails.js
@@ -37,8 +37,8 @@ function PetDetails(){
             </Grid>
 
             <Grid item container style={{marginTop: '24px'}}>
-                <Grid item xs={false} sm={2} />
-                <Grid direction="column" item container xs={12} sm={8}>
+                <Grid item xs={false} sm={4} />
+                <Grid direction="column" item container xs={12} sm={5}>
                     <Grid direction="column" item xs={12} sm={10} style={{fontSize: '20px', background: '#fff', fontWeight: '300'}}>
                         <h1 style={{fontWeight: '400', padding: '8px'}}>Meet {petInfo.name}!</h1>
                         <h2 style={{fontWeight: '300', padding: '8px'}}>I'm a {petInfo.size} sized {petInfo.breed}.</h2>
@@ -64,17 +64,17 @@ function PetDetails(){
             </Grid>
             
             <Grid item container style={{marginTop: '24px', marginBottom: '60px'}}>
-                <Grid item xs={false} sm={2} />
-                <Grid item container direction="column" xs={12} sm={8} >
+                <Grid item xs={false} sm={4} />
+                <Grid item container direction="column" xs={12} sm={5} >
                     <Grid item direction='column' xs={12} sm={10} style={{background: '#fff'}}>
                         <h1 style={{fontWeight: '400'}}>Any questions regarding {petInfo.name}?</h1>
                         <h2 style={{fontWeight: '400', paddingTop: '16px'}}>Contact us here</h2>
                     </Grid>
                     <Grid item container xs={12} sm={10} direction="column" style={{background: '#fff'}}>
                         <form>
-                            <input type='text' placeholder='Subject' style={{fontSize: '20px', width: '45%', margin: '8px 0', padding: '16px', background: 'transparent', outline: 'none', border: 'none', borderBottom: '1px solid black'}}/>
-                            <input type='email' placeholder='Email Address'  style={{fontSize: '20px', width: '45%', margin: '8px 0', padding: '16px', background: 'transparent', outline: 'none', border: 'none', borderBottom: '1px solid black', marginLeft: '10%'}}/>
-                            <textarea type='text' placeholder='Message'  style={{fontSize: '20px', fontFamily: 'Open Sans', fontWeight: '600', width: '100%', margin: '8px 0', paddingLeft: '16px', background: 'transparent', outline: 'none', border: 'none', borderBottom: '1px solid black'}}/>
+                            <input type='text' placeholder='First Name' style={{fontSize: '20px', width: '45%', margin: '8px 0', padding: '16px', background: 'transparent', outline: 'none', border: 'none', borderBottom: '1px solid black'}}/>
+                            <input type='email' placeholder='example@email.com'  style={{fontSize: '20px', width: '45%', margin: '8px 0', padding: '16px', background: 'transparent', outline: 'none', border: 'none', borderBottom: '1px solid black', marginLeft: '10%'}}/>
+                            <textarea type='text' placeholder='"Hi, I am wondering if..."'  style={{fontSize: '20px', fontFamily: 'Open Sans', fontWeight: '600', width: '100%', margin: '8px 0', paddingLeft: '16px', background: 'transparent', outline: 'none', border: 'none', borderBottom: '1px solid black'}}/>
                             <center>
                             <input type='submit' placeholder='submit' className='form-submit' onClick={submitHandler}/>
                             </center>
